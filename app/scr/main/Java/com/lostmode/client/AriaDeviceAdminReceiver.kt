01f0cf2adc6746b913b378d7b09e18f177215818
Dinos <143280137+Dinos17@users.noreply.github.com>
2025-08-31 21:59:59 +0300

    Update AriaDeviceAdminReceiver.kt

--- app/scr/main/Java/com/lostmode/client/AriaDeviceAdminReceiver.kt
+++ app/scr/main/Java/com/lostmode/client/AriaDeviceAdminReceiver.kt
@@ -1,1 +1,14 @@
-class AriaDeviceAdminReceiver : DeviceAdminReceiver()
+package com.lostmode.client
+
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+
+class AriaDeviceAdminReceiver : DeviceAdminReceiver() {
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+    }
+    override fun onDisabled(context: Context, intent: Intent) {
+        super.onDisabled(context, intent)
+    }
+}
