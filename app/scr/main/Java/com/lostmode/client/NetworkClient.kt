439904585d2f86602acf5053b10c5a656b2382d7
Dinos <143280137+Dinos17@users.noreply.github.com>
2025-08-31 22:00:20 +0300

    Update NetworkClient.kt

--- app/scr/main/Java/com/lostmode/client/NetworkClient.kt
+++ app/scr/main/Java/com/lostmode/client/NetworkClient.kt
@@ -1,1 +1,25 @@
+package com.lostmode.client
 
+import android.location.Location
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
+
+object NetworkClient {
+
+    private const val SERVER_URL = "http://YOUR_PC_IP:5000/update_location"
+
+    fun sendLocationToServer(location: Location) {
+        Thread {
+            try {
+                val url = URL("$SERVER_URL?lat=${location.latitude}&lng=${location.longitude}")
+                val conn = url.openConnection() as HttpURLConnection
+                conn.requestMethod = "GET"
+                conn.connect()
+                conn.inputStream.close()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }.start()
+    }
+}
