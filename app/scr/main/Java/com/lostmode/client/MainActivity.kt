fa44af2ba60334e66357424363e89ff4ddaf0f89
Dinos <143280137+Dinos17@users.noreply.github.com>
2025-08-31 21:58:53 +0300

    Update MainActivity.kt

--- app/scr/main/Java/com/lostmode/client/MainActivity.kt
+++ app/scr/main/Java/com/lostmode/client/MainActivity.kt
@@ -1,1 +1,28 @@
+package com.lostmode.client
 
+import android.app.Activity
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Activate Device Admin
+        val dpm = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+        val compName = ComponentName(this, AriaDeviceAdminReceiver::class.java)
+        val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName)
+        intent.putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "Required to enable Lost Mode")
+        startActivity(intent)
+
+        // Start LostModeService
+        val serviceIntent = Intent(this, LostModeService::class.java)
+        startForegroundService(serviceIntent)
+    }
+}
