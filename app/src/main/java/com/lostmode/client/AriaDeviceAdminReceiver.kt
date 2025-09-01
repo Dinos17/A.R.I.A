ed9d04995d546efa248f50a55197a931451becf7
Dinos <143280137+Dinos17@users.noreply.github.com>
2025-09-02 00:21:19 +0300

    Update AriaDeviceAdminReceiver.kt

--- app/src/main/java/com/lostmode/client/AriaDeviceAdminReceiver.kt
+++ app/src/main/java/com/lostmode/client/AriaDeviceAdminReceiver.kt
@@ -7,10 +7,10 @@ import android.widget.Toast
 
 class AriaDeviceAdminReceiver : DeviceAdminReceiver() {
     override fun onEnabled(context: Context, intent: Intent) {
-        Toast.makeText(context, "A.R.I.A. Device Admin enabled", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "ARIA device admin enabled", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDisabled(context: Context, intent: Intent) {
-        Toast.makeText(context, "A.R.I.A. Device Admin disabled", Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, "ARIA device admin disabled", Toast.LENGTH_SHORT).show()
     }
 }
