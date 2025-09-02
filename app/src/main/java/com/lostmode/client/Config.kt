514df3aeef707d7550e2bbe8a5b5d78e82023e5e
Dinos <143280137+Dinos17@users.noreply.github.com>
2025-09-02 12:30:00 +0300

    Update Config.kt

--- app/src/main/java/com/lostmode/client/Config.kt
+++ app/src/main/java/com/lostmode/client/Config.kt
@@ -2,16 +2,19 @@ package com.lostmode.client
 
 /**
  * Configuration for ARIA Client.
+ * 
+ * SERVER_BASE: Update this to your PC/server IP address.
+ * Example: "http://192.168.1.147:5000"
  *
- * Update SERVER_BASE to the IP shown by your Pydroid server.
- * Example: http://192.168.1.35:5000
- *
- * The client will POST location updates to SERVER_BASE + "/update".
+ * UPDATE_ENDPOINT: API endpoint to send location updates.
+ * COMMAND_ENDPOINT: API endpoint to fetch server commands.
  */
 object Config {
-    // Current server base URL (your Flask server on Pydroid)
-    const val SERVER_BASE = "http://192.168.1.102:5000"
-
-    // API endpoint for location updates
+    const val SERVER_BASE = "http://192.168.1.147:5000"
+    
+    // Endpoint to send device location updates
     const val UPDATE_ENDPOINT = "$SERVER_BASE/update"
+    
+    // Endpoint to fetch commands from server
+    const val COMMAND_ENDPOINT = "$SERVER_BASE/command"
 }
