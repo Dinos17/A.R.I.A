b16c5b3619e2ff35cfa7b39e5c4ba518c66f4cc8
Dinos <143280137+Dinos17@users.noreply.github.com>
2025-09-28 17:37:14 +0300

    Update DeviceAdapter.kt

--- app/src/main/java/com/lostmode/client/DeviceAdapter.kt
+++ app/src/main/java/com/lostmode/client/DeviceAdapter.kt
@@ -3,15 +3,11 @@ package com.lostmode.client
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
-/**
- * RecyclerView Adapter to display devices.
- *
- * @param devices List of Device objects to display
- * @param onClick Callback when a device is clicked
- */
 class DeviceAdapter(
     private val devices: List<Device>,
     private val onClick: (Device) -> Unit
@@ -19,6 +15,8 @@ class DeviceAdapter(
 
     inner class DeviceViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val txtName: TextView = view.findViewById(R.id.txtDeviceName)
+        val imgPhoto: ImageView = view.findViewById(R.id.imgDevicePhoto)
+
         init {
             view.setOnClickListener {
                 val position = adapterPosition
@@ -38,6 +36,17 @@ class DeviceAdapter(
     override fun getItemCount(): Int = devices.size
 
     override fun onBindViewHolder(holder: DeviceViewHolder, position: Int) {
-        holder.txtName.text = devices[position].name
+        val device = devices[position]
+        holder.txtName.text = device.name
+
+        // Load device photo using Glide if available
+        if (device.photoUrl.isNotEmpty()) {
+            Glide.with(holder.itemView.context)
+                .load(device.photoUrl)
+                .circleCrop()
+                .into(holder.imgPhoto)
+        } else {
+            holder.imgPhoto.setImageResource(R.drawable.ic_device_offline) // default icon
+        }
     }
 }
