3143b5b25662f451019f1d86606c025bf57f28d0
Dinos <143280137+Dinos17@users.noreply.github.com>
2025-09-30 00:12:39 +0300

    Update DeviceAdapter.kt

--- app/src/main/java/com/lostmode/client/DeviceAdapter.kt
+++ app/src/main/java/com/lostmode/client/DeviceAdapter.kt
@@ -1,17 +1,21 @@
 package com.lostmode.client
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 
 class DeviceAdapter(
     private val devices: List<NetworkClient.Device>,
     private val onClick: (NetworkClient.Device) -> Unit
 ) : RecyclerView.Adapter<DeviceAdapter.DeviceViewHolder>() {
+
+    companion object {
+        private const val TAG = "DeviceAdapter"
+    }
 
     inner class DeviceViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val txtName: TextView = view.findViewById(R.id.txtDeviceName)
@@ -21,7 +25,9 @@ class DeviceAdapter(
             view.setOnClickListener {
                 val position = adapterPosition
                 if (position != RecyclerView.NO_POSITION) {
-                    onClick(devices[position])
+                    val device = devices[position]
+                    Log.i(TAG, "Device clicked: ${device.id} / ${device.name}")
+                    onClick(device)
                 }
             }
         }
@@ -38,13 +44,11 @@ class DeviceAdapter(
     override fun onBindViewHolder(holder: DeviceViewHolder, position: Int) {
         val device = devices[position]
         holder.txtName.text = device.name
+        Log.d(TAG, "Binding device at position $position: ${device.id} / ${device.name}")
 
-        // Optional: display last update or coordinates in a subtitle
-        // holder.txtSubtitle.text = "Last: ${device.last_update}"
-
-        // Load device photo if available (currently server has no photoUrl)
+        // Default offline icon (update if server supports photo URLs)
         holder.imgPhoto.setImageResource(R.drawable.ic_device_offline)
-        // If you later add photoUrl to the server/device object, use Glide:
+        // Uncomment below if device.photoUrl is available
         // Glide.with(holder.itemView.context)
         //     .load(device.photoUrl)
         //     .circleCrop()
